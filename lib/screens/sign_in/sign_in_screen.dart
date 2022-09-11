eb0f370930b0b12985bf3dfac69a633572df6cd0
Razvan <40596596+RazvanRauta@users.noreply.github.com>
2022-09-11 18:28:54 +0200

    feat: added Sign in screen

---
+++ lib/screens/sign_in/sign_in_screen.dart
@@ -0,0 +1,18 @@
+import 'components/body.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  static String routeName = "/sign_in";
+
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: Body(),
+    );
+  }
+}
